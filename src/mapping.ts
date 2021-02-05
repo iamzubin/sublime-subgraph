0aa8eb623cf71696869449a040c8bc2861009057
prince mp <prince@marlin.pro>
2021-02-05 20:32:26 +0530

    lender and pool methods commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,23 +1,36 @@
 import {
-  Bytes, BigInt, store,
+  Bytes, store,
 } from "@graphprotocol/graph-ts";
 import {
-  Pool, User, LendingDetail,
+  Pool, LendingDetail,
 } from '../generated/schema';
 import {
+  BIGINT_ZERO,
   POOL_FACTORY_ADDRSS,
   LOAN_STATUS_CLOSED,
   LOAN_STATUS_TERMINATED,
   LOAN_STATUS_CANCELLED,
 } from "./utils/constants";
 import {
+  getLoanStatus,
+  updateUserPools,
+} from "./utils/helpers";
+import {
+  liquiditySupplied,
+  liquiditywithdrawn,
+  collateralRequested,
+} from '../generated/Lender/Lender';
+import {
   PoolCreated,
   openBorrowPoolClosed,
   openBorrowPoolTerminated,
   opneBorrowPoolCanceled,
+  extraCollateralDeposited,
+  withdrawnExtraCollateral,
+  LockedCollateral,
+  UnlockedCollateral,
   PoolFactory as PoolFactoryContract,
 } from '../generated/PoolFactory/PoolFactory';
-import { getLoanStatus } from "./utils/helpers";
 
 let poolFactoryContract = PoolFactoryContract.bind(
   POOL_FACTORY_ADDRSS,
@@ -32,7 +45,7 @@ export function handlePoolCreated(
     pool = new Pool(id);
   }
   pool.poolId = event.params.poolHash;
-  pool.poolOwner = event.params.poolCreator;
+  pool.poolOwner = event.params.poolCreator.toHexString();
 
   let poolDetails = poolFactoryContract.getPoolDetails(
     event.params.poolHash
@@ -40,15 +53,24 @@ export function handlePoolCreated(
 
   pool.borrowTokenType = poolDetails.value1;
   pool.collateralTokenType = poolDetails.value2;
-  pool.interestRate = poolDetails.value3;
+  pool.borrowRate = poolDetails.value3;
   pool.collateralRatio = poolDetails.value4;
+  pool.colleteralDeposited = BIGINT_ZERO;
+  pool.isPrivate = false;
   pool.loanStatus = getLoanStatus(
-    poolDetails.value6 as Bytes
+    poolDetails.value5 as Bytes
   );
-  pool.isExtensionRequested = poolFactoryContract
+  pool.extensionRequested = poolFactoryContract
     .getPoolDetails2(event.params.poolHash);
 
   pool.save();
+
+  updateUserPools(
+    event.params.poolCreator,
+    event.params.poolHash.toHexString(),
+    "created",
+    "borrow-pool"
+  );
 }
 
 export function handleOpenBorrowPoolClosed(
@@ -81,5 +103,136 @@ export function handleOpneBorrowPoolCanceled(
   );
 
   pool.loanStatus = LOAN_STATUS_CANCELLED;
+  pool.save();
+}
+
+export function handleExtraCollateralDeposited(
+  event: extraCollateralDeposited
+): void {
+  let pool = Pool.load(
+    event.params.PoolHash.toHexString()
+  );
+
+  pool.colleteralDeposited = pool.collectedAmount
+    .plus(event.params.ExtraCollateralAmount);
+
+  pool.save();
+}
+
+export function handleWithdrawnExtraCollateral(
+  event: withdrawnExtraCollateral
+): void {
+  let pool = Pool.load(
+    event.params.poolHash.toHexString()
+  );
+
+  pool.colleteralDeposited = pool.collectedAmount
+    .minus(event.params.WithDrawCollateralAmount);
+
+  pool.save();
+}
+
+export function handleLockedCollateral(
+  event: LockedCollateral
+): void {
+  let pool = Pool.load(
+    event.params.poolId.toHexString()
+  );
+
+  pool.vaultShares = pool.vaultShares
+    .plus(event.params.lpTokensReceived);
+
+  pool.save();
+}
+
+export function handleUnlockedCollateral(
+  event: UnlockedCollateral
+): void {
+  let pool = Pool.load(
+    event.params.poolId.toHexString()
+  );
+
+  pool.vaultShares = pool.vaultShares
+    .minus(event.params.collateralReceived);
+
+  pool.save();
+}
+
+export function handleLiquiditySupplied(
+  event: liquiditySupplied
+): void {
+  let lendingDetailId = event.params.poolId.toHexString() +
+    event.params.lenderAddress.toHexString();
+
+  let lendingDetail = LendingDetail.load(
+    lendingDetailId
+  );
+
+  if (lendingDetail == null) {
+    lendingDetail = new LendingDetail(lendingDetailId);
+  }
+
+  lendingDetail.pool = event.params.poolId.toHexString();
+  lendingDetail.collateralCalled = false;
+  lendingDetail.amountSupplied = lendingDetail
+    .amountSupplied.plus(event.params.amountSupplied);
+
+  lendingDetail.save();
+
+  updateUserPools(
+    event.params.lenderAddress,
+    event.params.poolId.toHexString(),
+    "supplied",
+    "lending-pool"
+  );
+
+  let pool = Pool.load(event.params.poolId.toHexString());
+  pool.collectedAmount = pool.collectedAmount
+    .plus(event.params.amountSupplied);
+
+  pool.save();
+}
+
+export function handleLiquiditywithdrawn(
+  event: liquiditywithdrawn
+): void {
+  let lendingDetailId = event.params.poolId.toHexString() +
+    event.params.lenderAddress.toHexString();
+
+  let lendingDetail = LendingDetail.load(lendingDetailId);
+
+  lendingDetail.amountWithdrawn = lendingDetail.amountWithdrawn
+    .plus(event.params.amount);
+
+  if (
+    lendingDetail.amountWithdrawn ==
+    lendingDetail.amountSupplied
+  ) {
+    store.remove('LendingDetail', lendingDetailId);
+  } else {
+    lendingDetail.save();
+  }
+
+  let pool = Pool.load(event.params.poolId.toHexString());
+  pool.collectedAmount = pool.collectedAmount
+    .minus(event.params.amount);
+
+  pool.save();
+}
+
+export function handleCollateralRequested(
+  event: collateralRequested
+): void {
+  let lendingDetailId = event.params.poolID.toHexString() +
+    event.params.lenderAddress.toHexString();
+
+  let lendingDetail = LendingDetail.load(lendingDetailId);
+  lendingDetail.collateralCalled = true;
+  lendingDetail.save();
+
+  let pool = Pool.load(event.params.poolID.toHexString());
+  pool.collateralCalls = pool.collateralCalls
+    .plus(lendingDetail.amountSupplied);
+
   pool.save();
 }
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,5 @@
 import { Bytes } from "@graphprotocol/graph-ts";
+import { User } from "../../generated/schema";
 import {
     LOAN_STATUS_ACTIVE,
     LOAN_STATUS_CLOSED,
@@ -6,6 +7,7 @@ import {
     LOAN_STATUS_CANCELLED,
     LOAN_STATUS_INACTIVE,
     LOAN_STATUS_TERMINATED,
+    STATUS_UNVERIFIED,
 } from './constants';
 
 export function getLoanStatus(
@@ -27,4 +29,36 @@ export function getLoanStatus(
         default:
             return LOAN_STATUS_DEFAULTED;
     }
+}
+
+export function updateUserPools(
+    userAddress: Bytes,
+    poolId: string,
+    action: string,
+    poolType: string,
+): void {
+    let userId = userAddress.toHexString()
+    let user = User.load(userId);
+
+    if (user == null) {
+        user = new User(userId);
+        user.address = userAddress;
+        user.lendingPools = [];
+        user.borrowingPools = [];
+        user.status = STATUS_UNVERIFIED;
+    }
+
+    if (action == "created" || action == "supplied") {
+        if (poolType == "borrow-pool") {
+            let borrowingPools = user.borrowingPools;
+            borrowingPools.push(poolId);
+            user.borrowingPools = borrowingPools;
+        } else if (poolType == "lending-pool") {
+            let lendingPools = user.lendingPools;
+            lendingPools.push(poolId);
+            user.lendingPools = lendingPools;
+        }
+    }
+
+    user.save();
 }
