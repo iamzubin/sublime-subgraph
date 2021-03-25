3822c91c057c298eeedfd3cd22102ab0dd9ae966
Aditya Garg <aditya26026@gmail.com>
2021-03-25 11:08:12 +0530

    edited poolCreated

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,23 +1,14 @@
-import { PoolFactory } from '../generated/schema';
+import { Pool } from '../generated/schema';
 import {
   PoolCreated,
 } from '../generated/PoolFactory/PoolFactory';
-
 export function handlePoolCreated(
   event: PoolCreated
 ): void {
-  
-  let poolFactory = PoolFactory.load('0')
-  if (poolFactory == null){
-    poolFactory = new PoolFactory('0');
-    poolFactory.pools = [];
-    poolFactory.borrowers = []
+  let pool = Pool.load(event.params.pool.toHexString())
+  if (pool == null){
+    pool = new Pool(event.params.pool.toHexString());
   }
-  let pools = poolFactory.pools
-  pools.push(event.params.pool.toHexString());
-  poolFactory.pools = pools
-  let borrowers = poolFactory.borrowers
-  borrowers.push(event.params.borrower.toHexString());
-  poolFactory.borrowers = borrowers
-  poolFactory.save()
+  pool.borrower = event.params.borrower.toHexString();
+  pool.save()
 }
