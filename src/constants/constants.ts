e85c38791a4853dcfa6539e620e3d93419ef0ac0
Prateek Reddy <prateekreddyy@gmail.com>
2021-11-12 12:30:20 +0530

    handle savings account balances related events

---
+++ src/constants/constants.ts
@@ -0,0 +1,3 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+
+export let BIGINT_ZERO = BigInt.fromI32(0);
--- src/savingsAccount/savingsAccount.ts
+++ src/savingsAccount/savingsAccount.ts
@@ -1,3 +1,4 @@
+import { Address } from "@graphprotocol/graph-ts";
 import { 
     Approved, 
     CreditLineAllowanceRefreshed, 
@@ -9,20 +10,21 @@ import {
     Withdrawn,
     WithdrawnAll,
 } from "../../generated/SavingsAccount/SavingsAccount";
+import { addBalanceToUser, decreaseBalance, getBalance, getUserBalance, increaseBalance } from "./helpers";
 
 export function handleDeposit(event: Deposited): void {
-
+    increaseBalance(event.params.user, event.params.token, event.params.strategy, event.params.sharesReceived);
 }
 
 export function handleApproval(event: Approved): void {
-
+    
 }
 
 export function handleCreditLineAllowanceRefreshed(event: CreditLineAllowanceRefreshed): void {
 
 }
 
-export function handleCreditLineUpdated(event: CreditLineUpdated): void {
+export function handleCreditLineUpdate(event: CreditLineUpdated): void {
 
 }
 
@@ -31,17 +33,26 @@ export function handleStrategyRegistryUpdate(event: StrategyRegistryUpdated): vo
 }
 
 export function handleStrategySwitched(event: StrategySwitched): void {
-
+    increaseBalance(event.params.user, event.params.token, event.params.currentStrategy, event.params.sharesDecreasedInCurrentStrategy);
+    decreaseBalance(event.params.user, event.params.token, event.params.newStrategy, event.params.sharesIncreasedInNewStrategy);
 }
 
 export function handleTransfer(event: Transfer): void {
-
+    increaseBalance(event.params.from, event.params.token, event.params.strategy, event.params.amount);
+    decreaseBalance(event.params.to, event.params.token, event.params.strategy, event.params.amount);
 }
 
 export function handleWithdraw(event: Withdrawn): void {
-
+    decreaseBalance(event.params.from, event.params.token, event.params.strategy, event.params.sharesWithdrawn);
 }
 
 export function handleWithdrawAll(event: WithdrawnAll): void {
-    
+    let userBalance = getUserBalance(event.params.user, event.params.token);
+    let strategyBalances = userBalance.strategyBalance;
+
+    for(let i=0; i < strategyBalances.length; i++) {
+        let strategy = Address.fromHexString(strategyBalances[i]);
+        let balance = getBalance(event.params.user, event.params.token, strategy);
+        decreaseBalance(event.params.user, event.params.token, strategy, balance.shares);
+    }
 }
