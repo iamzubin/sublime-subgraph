01eef731f7ddf044db552de22996d1739e67da4f
Prateek Reddy <prateekreddyy@gmail.com>
2021-11-12 08:36:25 +0530

    event handlers structure added

---
+++ src/savingsAccount/savingsAccount.ts
@@ -0,0 +1,47 @@
+import { 
+    Approved, 
+    CreditLineAllowanceRefreshed, 
+    CreditLineUpdated, 
+    Deposited, 
+    StrategyRegistryUpdated, 
+    StrategySwitched,
+    Transfer,
+    Withdrawn,
+    WithdrawnAll,
+} from "../../generated/SavingsAccount/SavingsAccount";
+
+export function handleDeposit(event: Deposited): void {
+
+}
+
+export function handleApproval(event: Approved): void {
+
+}
+
+export function handleCreditLineAllowanceRefreshed(event: CreditLineAllowanceRefreshed): void {
+
+}
+
+export function handleCreditLineUpdated(event: CreditLineUpdated): void {
+
+}
+
+export function handleStrategyRegistryUpdate(event: StrategyRegistryUpdated): void {
+
+}
+
+export function handleStrategySwitched(event: StrategySwitched): void {
+
+}
+
+export function handleTransfer(event: Transfer): void {
+
+}
+
+export function handleWithdraw(event: Withdrawn): void {
+
+}
+
+export function handleWithdrawAll(event: WithdrawnAll): void {
+    
+}
