f0f71f0b177d97ed2e9960f7013062497890dbca
Akshay <akshay.111meher@gmail.com>
2021-11-24 20:41:26 +0530

    Change address in the contracts

--- src/contractAddresses.ts
+++ src/contractAddresses.ts
@@ -1,3 +1,3 @@
 import { Address } from "@graphprotocol/graph-ts";
 
-export let CREDIT_LINE_ADDRESS = Address.fromString("0x370707EFEfC8375d45460A0DCFAFb88C0d3A9669");
+export let CREDIT_LINE_ADDRESS = Address.fromString("0xA102E6f5Bda8d1222520345a759D298B68938e5C");
