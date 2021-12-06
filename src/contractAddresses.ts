8420cdd0743a9f40c9e86a1b68cde5945e65b0be
KalpitaMandal <kalpita1703@gmail.com>
2021-12-06 13:47:37 +0530

    Adding basic functions for the profile page

--- src/contractAddresses.ts
+++ src/contractAddresses.ts
@@ -1,3 +1,5 @@
 import { Address } from "@graphprotocol/graph-ts";
 
 export let CREDIT_LINE_ADDRESS = Address.fromString("0xA102E6f5Bda8d1222520345a759D298B68938e5C");
+
+export let VERIFICATION_ADDRESS = Address.fromString("0x4256a2722d60c90c39515E0Adb3E731b91480996");
